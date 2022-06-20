70d3cf4f1f1461c65b7b45f6e876f3cec28a3719
Sahil <sahil.saiwal.31@gmail.com>
2022-06-20 11:27:10 +0530

    Updated New Final Repo

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+if (true) {
+  module.exports = require("./prod");
+}
---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  MONGOURI:
+    "mongodb+srv://ashish:ashish098@cluster0.jjgz2u0.mongodb.net/?retryWrites=true&w=majority",
+  JWT_SECRET: "ashish",
+  SENDGRID_API: process.env.SENDGRID_API,
+  EMAIL: "sudhanshukumawat11@gmail.com",
+};
